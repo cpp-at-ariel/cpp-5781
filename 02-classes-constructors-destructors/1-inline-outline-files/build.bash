d797c00ce3947c1007fc26841bcc53d66bf6b7ed
Erel Segal-Halevi cron <erelsgl@gmail.com>
2021-03-08 18:48:55 +0200

    start making order in week 2

--- 02-classes-constructors-destructors/1-inline-outline-files/build.bash
+++ 02-classes-constructors-destructors/1-inline-outline-files/build.bash
@@ -1,6 +1,6 @@
 #!bash
-clang++-5.0 -std=c++17 --compile Complex.cpp -o Complex.o
-clang++-5.0 -std=c++17 --compile main.cpp -o main.o
-clang++-5.0 -std=c++17 Complex.o main.o
+clang++-9 -std=c++2a --compile Complex.cpp -o Complex.o
+clang++-9 -std=c++2a --compile main.cpp -o main.o
+clang++-9 -std=c++2a Complex.o main.o
 ./a.out
 
