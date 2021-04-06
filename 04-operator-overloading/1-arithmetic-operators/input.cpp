c82fd3f4c027f6134efe6463043bb6e27d1572b2
Erel Segal-Halevi cron <erelsgl@gmail.com>
2021-04-06 15:21:09 +0300

    write next char

--- 04-operator-overloading/1-arithmetic-operators/input.cpp
+++ 04-operator-overloading/1-arithmetic-operators/input.cpp
@@ -15,11 +15,16 @@ using namespace std;
 int main() {
     int i=9999;
     cout << i << endl;
-    istringstream input("123");
+    istringstream input("123 x");
     cout << "Before reading: ";
-    if (input) {cout <<"input is OK"<<endl;} else {cout << "input is wrong"<<endl;}
+    if (input) {cout <<"input is OK"<<endl;} else {cout << "input is bad"<<endl;}
     input >> i;
     cout << "After reading: ";
-    if (input) {cout <<"input is OK"<<endl;} else {cout << "input is wrong"<<endl;}
+    if (input) {cout <<"input is OK"<<endl;} else {cout << "input is bad"<<endl;}
     cout << i << endl;
+
+    input.clear();
+    char nextChar = '#';
+    input >> nextChar;
+    cout << "next char is: " << nextChar << endl;
 }
