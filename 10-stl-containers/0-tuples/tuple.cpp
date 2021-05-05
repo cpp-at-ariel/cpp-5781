e8f6825ac22ddbd068df6f234391be4f67655afe
Erel Segal-Halevi cron <erelsgl@gmail.com>
2021-05-06 01:24:46 +0300

    tuple

--- 10-stl-containers/0-tuples/tuple.cpp
+++ 10-stl-containers/0-tuples/tuple.cpp
@@ -9,19 +9,19 @@
 #include <tuple>
 using namespace std;
 
-tuple<int,char,string> f_full() {
+tuple<int,char,string> f_full() {                    // Long style
 	return tuple<int,char,string>{5,'a',"hello"};
 }
 
-tuple<int,char,string> f_automatic_constructor() {
+tuple<int,char,string> f_automatic_constructor() {   // Best style
 	return {5,'a',"hello"};
 }
 
-auto f_automatic_returntype() {
+auto f_automatic_returntype() {                      
 	return tuple<int,char,string>{5,'a',"hello"};
 }
 
-auto f_automatic_both() {
+auto f_automatic_both() {       // Short style
 	// since c++17:
  	return tuple{5,'a',"hello"};
 
@@ -33,25 +33,27 @@ auto f_automatic_both() {
 // 	return {5,'a',"hello"};
 // }
 
-auto f_automatic_bug() {
+auto f_automatic_bug() {        // Python style (does not work)
 	return (5,'a',"bug!");
 }
 
+// auto all_functions() {
+// 	return tuple{f_full, f_automatic_constructor, f_automatic_returntype, f_automatic_both};
+// }
 
 int main() {
-	auto p = pair(6,'b');
+	auto p = pair{6,'b'};
 	auto [p0,p1]  = p;
 
-	auto t = tuple(5,'a', 6.7);  // equivalent to:
-	// tuple<int,char,double> t = tuple(5,'a', 6.7);  // equivalent to:
+	auto t = tuple{5,'a', 6.7};  // equivalent to:
+	// tuple<int,char,double> t = tuple(5,'a', 6.7);
 	cout << get<0>(t) << " " << get<1>(t) << " " << get<2>(t) << endl;
 	auto [t0,t1,t2] = t;
 
 	auto [x0,x1,_] = t;
 
 
-	auto f = f_automatic_both; // f is a pointer to function
-	auto [ii,cc,ss] = f();
+	auto [ii,cc,ss] = f_automatic_both();
 	// Equivalent to:
 
 	// auto tt = f();
@@ -60,6 +62,8 @@ int main() {
 	// auto ss = get<2>(tt);
 
 	cout << ii << " " << cc << " " << ss << endl;
+
+	// auto af = all_functions();
 
 	cout << f_automatic_bug() << endl;
 }
